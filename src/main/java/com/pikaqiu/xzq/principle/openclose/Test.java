48de8ea6edae3da67ff1a89d36fbec06fa038582
pikaqiu <1047984367@qq.com>
2018-12-30 23:13:57 +0800

    第一次

--- src/main/java/com/pikaqiu/xzq/principle/openclose/Test.java
+++ src/main/java/com/pikaqiu/xzq/principle/openclose/Test.java
@@ -9,10 +9,12 @@ import com.pikaqiu.xzq.principle.openclose.impl.JavaCourse;
  * @create: 2018-12-30 20:29
  **/
 public class Test {
+
     public static void main(String[] args) {
         JavaCourse javaCourse = new JavaCourse(11, "java课程", 42d);
         System.out.println(javaCourse.getName());
         System.out.println("课程Id：" + javaCourse.getId() + "       课程名：" + javaCourse.getName() + "      课程价格：" + javaCourse.getPrice());
-
     }
+
+
 }
